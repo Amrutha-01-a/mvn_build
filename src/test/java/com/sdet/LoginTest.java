7a03a0f273900c2bbc8bb0c801d02c58310709f4
Amrutha_Acharya <128776932+Amrutha-01-a@users.noreply.github.com>
2023-06-17 11:26:41 +0530

    Update LoginTest.java

--- src/test/java/com/sdet/LoginTest.java
+++ src/test/java/com/sdet/LoginTest.java
@@ -1,67 +1,33 @@
-<<<<<<< HEAD
-//package com.sdet;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//
-//public class LoginTest {
-//
-//@Test
-//	
-//public void Login_Tc() 
-//	{
-//		WebDriverManager.chromedriver().setup();
-//		ChromeDriver driver = new ChromeDriver();
-//		
-//		driver.get("https://opensource-demo.orangehrmlive.com/");
-//		driver.manage().window().maximize();
-//		
-//		
-//		
-//		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-//		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-//		driver.findElement(By.id("btnLogin")).click();
-//		
-//		Assert.assertEquals(driver.getTitle(), "OrangeHRM");
-//		driver.close();
-//		 
-//		}
-//
-//}
-=======
-// package com.sdet;
-// import org.openqa.selenium.By;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.testng.Assert;
-// import org.testng.annotations.Test;
+package com.sdet;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-// public class LoginTest {
+public class LoginTest {
 
-// @Test
+@Test
 	
-// public void Login_Tc() 
-// 	{
-// 		WebDriverManager.chromedriver().setup();
-// 		ChromeDriver driver = new ChromeDriver();
+public void Login_Tc() 
+	{
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
 		
-// 		driver.get("https://opensource-demo.orangehrmlive.com/");
-// 		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.manage().window().maximize();
 		
 		
 		
-// 		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-// 		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-// 		driver.findElement(By.id("btnLogin")).click();
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
 		
-// 		Assert.assertEquals(driver.getTitle(), "OrangeHRM");
-// 		driver.close();
+		Assert.assertEquals(driver.getTitle(), "OrangeHRM");
+		driver.close();
 		 
-// 		}
+		}
 
-// }
->>>>>>> 8fd5f1b72b309237f213748edc87165fdc7cde17
+}
+
